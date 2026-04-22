342a4a348c257e828a634bdbae54ddfd3bc81176
Eric Stephenson <ericb.stephenson@gmail.com>
2026-04-22 15:06:46 -0400

    add auth guards to BlogDelete

--- app/graphql/mutations/blog_delete.rb
+++ app/graphql/mutations/blog_delete.rb
@@ -9,16 +9,27 @@ module Mutations
     argument :id, ID, required: true
 
     def resolve(id:)
+      raise_login_error unless context[:current_user]
+      raise_authorization_error unless context[:current_user].admin?
+
       blog = ::Blog.find(id)
-      raise_execution_error(blog) unless blog.destroy!
+      raise_deletion_error(blog) unless blog.destroy!
 
       { message: "Blog deleted successfully" }
     end
 
     private
 
-    def raise_execution_error(blog)
-      raise GraphQL::ExecutionError.new "Error deleting blog", extensions: blog.errors.to_hash
+    def raise_login_error
+      raise GraphQL::ExecutionError, "Login to access"
+    end
+
+    def raise_authorization_error
+      raise GraphQL::ExecutionError, "Only admins can delete blogs"
+    end
+
+    def raise_deletion_error(blog)
+      raise GraphQL::ExecutionError.new("Error deleting blog", extensions: blog.errors.to_hash)
     end
   end
 end
