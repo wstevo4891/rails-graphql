075cb7e9dd7e8b3fea665cc56f4c6d0d9cad35d7
Eric Stephenson <ericb.stephenson@gmail.com>
2026-04-22 14:03:08 -0400

    add password validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
+# frozen_string_literal: true
+
+# Model for users table
 class User < ApplicationRecord
+  PASSWORD_REGEXP = /\A
+    (?=.{8,})
+    (?=.*\d)
+    (?=.*[a-z])
+    (?=.*[A-Z])
+    (?=.*[[:^alnum:]])
+  /x.freeze
+
   has_many :blogs, dependent: :destroy
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, format: { with: PASSWORD_REGEXP, message: "condition failed" }
+
   before_save { self.email = email.downcase }
 end
