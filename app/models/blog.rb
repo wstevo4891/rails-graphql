16037aa83ea460cc3f68e69296a0a27090b1ac15
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-09 11:50:19 -0400

    add validations to Blog model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
+
+  validates_associated :user
+  validates :title, presence: true, length: { in: 1..100 }
+  validates :description, presence: true, length: { in: 1..500 }
 end
