e4d8a37be7f3e6f2b88e8c9657efc87b801b1fe5
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-09 19:10:45 -0400

    set default nil values for title and description in BlogUpdate#resolve so each attribute can be updated individually

--- app/graphql/mutations/blog_update.rb
+++ app/graphql/mutations/blog_update.rb
@@ -10,8 +10,10 @@ module Mutations
     argument :title, String, required: false
     argument :description, String, required: false
 
-    def resolve(id:, title:, description:)
+    def resolve(id:, title: nil, description: nil)
       blog = ::Blog.find(id)
+      title = blog.title if title.nil?
+      description = blog.description if description.nil?
       raise_execution_error(blog) unless blog.update(title:, description:)
 
       { blog: blog }
@@ -20,7 +22,10 @@ module Mutations
     private
 
     def raise_execution_error(blog)
-      raise GraphQL::ExecutionError.new "Error updating blog", extensions: blog.errors.to_hash
+      raise GraphQL::ExecutionError.new(
+        "Error updating blog",
+        extensions: blog.errors.to_hash
+      )
     end
   end
 end
