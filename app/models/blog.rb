27fb2b5518cd7faa76887d4fb8fe8c8faf42e794
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-09 12:01:23 -0400

    add max length constants in Blog model, update spec

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,10 @@
 class Blog < ApplicationRecord
+  TITLE_MAX = 100
+  DESCRIPTION_MAX = 500
+
   belongs_to :user
 
   validates_associated :user
-  validates :title, presence: true, length: { in: 1..100 }
-  validates :description, presence: true, length: { in: 1..500 }
+  validates :title, presence: true, length: { in: 1..TITLE_MAX }
+  validates :description, presence: true, length: { in: 1..DESCRIPTION_MAX }
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -16,9 +16,7 @@ RSpec.describe Blog, type: :model do
   describe "validations" do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
-  end
 
-  describe "edge cases" do
     it "is invalid without a title" do
       blog.title = ""
 
@@ -26,11 +24,27 @@ RSpec.describe Blog, type: :model do
       expect(blog.errors[:title]).to include("can't be blank")
     end
 
+    it "is invalid when title is too long" do
+      message = "is too long (maximum is #{Blog::TITLE_MAX} characters)"
+      blog.title = "A" * (Blog::TITLE_MAX + 1)
+
+      expect(blog).not_to be_valid
+      expect(blog.errors[:title]).to include(message)
+    end
+
     it "is invalid without a description" do
       blog.description = ""
 
       expect(blog).not_to be_valid
       expect(blog.errors[:description]).to include("can't be blank")
+    end
+
+    it "is invalid when description is too long" do
+      message = "is too long (maximum is #{Blog::DESCRIPTION_MAX} characters)"
+      blog.description = "A" * (Blog::DESCRIPTION_MAX + 1)
+
+      expect(blog).not_to be_valid
+      expect(blog.errors[:description]).to include(message)
     end
 
     it "is invalid without a user" do
